c5608febdcf96f9de0cd7f1cb7709330d1695cfc
Ana Lima <analimaps@icloud.com>
2022-12-13 18:12:42 -0500

    Package class - Problem with Python solved

--- jaffle_shop/models/staging/jaffle_shop_schema/all_days.sql
+++ jaffle_shop/models/staging/jaffle_shop_schema/all_days.sql
@@ -1,7 +1,12 @@
+/*This block creates a table (insteade of a view) */
+{{ config (
+    materialized="table"
+)}}
+
 {{ dbt_utils.date_spine(
     datepart="day",
-    start_date="cast('2019-01-01' as date)",
-    end_date="cast('2020-01-01' as date)"
+    start_date="cast('2018-01-01' as date)",
+    end_date="cast('2019-01-01' as date)"
    )
 }}
 
--- jaffle_shop/models/staging/jaffle_shop_schema/testing_package.sql
+++
@@ -1,3 +0,0 @@
-select order_date, 
-       {{ dbt_date.day_of_month(order_date) }} as day_of_month
-from {{ source('jaffle_shop','orders') }}
